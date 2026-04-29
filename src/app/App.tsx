c64e7abfae0d8448eccd41bf96a4c3805956c4b8
oyvind-st <oyvind@adseo.no>
2026-04-29 21:37:40 +0200

    Fix App.tsx

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,6 +2,8 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { DarkModeProvider } from './contexts/DarkModeContext'
 import { MVPProvider } from './contexts/MVPContext'
 import { Layout } from './components/Layout'
+
+// Supabase-connected MVP components
 import { DashboardMVP } from './components/mvp/Dashboard.mvp'
 import { CustomerListMVP } from './components/mvp/CustomerList.mvp'
 import { CustomerDetailMVP } from './components/mvp/CustomerDetail.mvp'
@@ -10,20 +12,28 @@ import { TicketListMVP } from './components/mvp/TicketList.mvp'
 import { LeveranserListMVP } from './components/mvp/LeveranserList.mvp'
 import { LeadListMVP } from './components/mvp/LeadList.mvp'
 import { SettingsMVP } from './components/mvp/Settings.mvp'
+
 export default function App() {
   return (
-    <DarkModeProvider><MVPPevider><BrowserRouter>
-      <Routes><Route path="/" element={<Layout />}>
-        <Route index element={<DashboardMVP />} />
-        <Route path="customers" element={<CustomerListMVP />} />
-        <Route path="customers/:id" element={<CustomerDetailMVP />} />
-        <Route path="tasks" element={<TaskListMVP />} />
-        <Route path="tickets" element={<TicketListMVP />} />
-        <Route path="leveranser" element={<LeveranserListMVP />} />
-        <Route path="leads" element={<LeadListMVP />} />
-        <Route path="settings" element={<SettingsMVP />} />
-        <Route path="*" element={<DashboardMVP />} />
-      </Route></Routes>
-    </BrowserRouter></MVPProvider></DarkModeProvider>
+    <DarkModeProvider>
+      <MVPProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Layout />}>
+              <Route index element={<DashboardMVP />} />
+              <Route path="customers" element={<CustomerListMVP />} />
+              <Route path="customers/:id" element={<CustomerDetailMVP />} />
+              <Route path="tasks" element={<TaskListMVP />} />
+              <Route path="tickets" element={<TicketListMVP />} />
+              <Route path="tickets/:id" element={<TicketListMVP />} />
+              <Route path="leveranser" element={<LeveranserListMVP />} />
+              <Route path="leads" element={<LeadListMVP />} />
+              <Route path="settings" element={<SettingsMVP />} />
+              <Route path="*" element={<DashboardMVP />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </MVPProvider>
+    </DarkModeProvider>
   )
 }
